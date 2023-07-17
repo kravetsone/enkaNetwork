e612ac31398a903de2eceed95d8f138a1a041d0d
Kravets <57632712+kravetsone@users.noreply.github.com>
2023-07-18 01:01:08 +0300

    add: assetsUpdater which automatically updates data & localization assets

---
+++ src/helpers/assetsManager/constants.ts
@@ -0,0 +1,67 @@
+import { TLanguage } from "../../types";
+
+export const PROJECT_GITLAB_URL =
+    "https://gitlab.com/api/v4/projects/41287973/repository/commits";
+export const BASE_URL =
+    "https://gitlab.com/Dimbreath/AnimeGameData/-/raw/master";
+export const LOCALIZATION_BASE_URL = BASE_URL + "/TextMap/TextMap";
+
+export const CHARACTER_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/AvatarExcelConfigData.json";
+export const SKILLSET_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/AvatarSkillDepotExcelConfigData.json";
+export const SKILL_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/AvatarSkillExcelConfigData.json";
+export const CONSTELLATION_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/AvatarTalentExcelConfigData.json";
+export const COSTUME_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/AvatarCostumeExcelConfigData.json";
+export const NAMECARD_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/MaterialExcelConfigData.json";
+export const WEAPON_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/WeaponExcelConfigData.json";
+export const RELIQUARY_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/ReliquaryExcelConfigData.json";
+export const RELIQUARY_SET_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/ReliquarySetExcelConfigData.json";
+export const RELIQUARY_AFFIX_DATA_URL =
+    BASE_URL + "/ExcelBinOutput/EquipAffixExcelConfigData.json";
+
+export const qualityTypesStars = {
+    QUALITY_ORANGE: 5,
+    QUALITY_PURPLE: 4,
+};
+
+export const elements = {
+    Ice: "Cryo",
+    Water: "Hydro",
+    Wind: "Anemo",
+    Fire: "Pyro",
+    Rock: "Geo",
+    Electric: "Electro",
+    Grass: "Dendro",
+};
+
+export const excludeCharacters = [
+    10000001, 10000005, 10000007, 11000008, 11000009, 11000010, 11000011,
+    11000013, 11000017, 11000018, 11000019, 11000025, 11000026, 11000027,
+    11000028, 11000030, 11000031, 11000032, 11000033, 11000034, 11000035,
+    11000036, 11000037, 11000038, 11000039, 11000040, 11000041, 11000042,
+    11000043, 11000044, 11000045,
+];
+
+export const localizationLanguages: TLanguage[] = [
+    "CHS",
+    "CHT",
+    "DE",
+    "EN",
+    "ES",
+    "FR",
+    "ID",
+    "JP",
+    "KR",
+    "PT",
+    "RU",
+    "TH",
+    "VI",
+];
---
+++ src/types/parseDataTypes.ts
@@ -0,0 +1,169 @@
+export interface ICharacterExcelData {
+    useType: string;
+    bodyType: string;
+    scriptDataPathHash: number;
+    iconName: string;
+    sideIconName: string;
+    qualityType: "QUALITY_ORANGE" | "QUALITY_PURPLE";
+    chargeEfficiency: number;
+    LBODGAEPDMJ: number;
+    initialWeapon: number;
+    weaponType: string;
+    NIPILHLOMDG: number;
+    imageName: string;
+    gachaCardNameHash: number;
+    manekinPathHash: number;
+    cutsceneShow: string;
+    skillDepotId: number;
+    staminaRecoverSpeed: number;
+    candSkillDepotIds: any[];
+    manekinJsonConfigHash: number;
+    manekinMotionConfig: number;
+    descTextMapHash: number;
+    avatarIdentityType: string;
+    avatarPromoteId: number;
+    avatarPromoteRewardLevelList: number[];
+    avatarPromoteRewardIdList: number[];
+    featureTagGroupID: number;
+    infoDescTextMapHash: number;
+    hpBase: number;
+    attackBase: number;
+    defenseBase: number;
+    critical: number;
+    criticalHurt: number;
+    propGrowCurves: IPropGrowCurve[];
+    BCAGNCKHFOF: number;
+    prefabPathHash: number;
+    id: number;
+    nameTextMapHash: number;
+    NOLCALJMBOB: number;
+    prefabPathRemoteHash: number;
+    controllerPathHash: number;
+    controllerPathRemoteHash: number;
+    LODPatternName: string;
+}
+
+export interface IPropGrowCurve {
+    type: string;
+    growCurve: string;
+}
+
+export interface ISkillsetExcelData {
+    id: number;
+    energySkill: number;
+    skills: number[];
+    subSkills: number[];
+    extraAbilities: string[];
+    talents: number[];
+    talentStarName: string;
+    inherentProudSkillOpens: unknown[];
+    skillDepotAbilityGroup: string;
+}
+
+export interface ISkillExcelData {
+    id: number;
+    nameTextMapHash: number;
+    abilityName: string;
+    descTextMapHash: number;
+    skillIcon: string;
+    cdTime: number;
+    costElemType:
+        | "Ice"
+        | "Water"
+        | "Wind"
+        | "Fire"
+        | "Rock"
+        | "Electric"
+        | "Grass";
+    costElemVal: number;
+    maxChargeNum: number;
+    triggerID: number;
+    lockShape: string;
+    lockWeightParams: number[];
+    isAttackCameraLock: boolean;
+    buffIcon: string;
+    proudSkillGroupId: number;
+    globalValueKey: string;
+}
+
+export interface IConstellationExcelData {
+    talentId: number;
+    nameTextMapHash: number;
+    descTextMapHash: number;
+    icon: string;
+    mainCostItemId: number;
+    mainCostItemCount: number;
+    openConfig: string;
+    addProps: unknown[];
+    paramList: number[];
+}
+
+export interface ICostumeExcelData {
+    KKGNHHIFAMD: number;
+    BOKNDNFKIGE: number;
+    nameTextMapHash: number;
+    descTextMapHash: number;
+    itemId: number;
+    PIJICPMEBIP: number;
+    jsonName: string;
+    JGPBEIOHAPP: number;
+    JBEMKLEMLOB: number;
+    IEHAIEJOHKJ: number;
+    PGCDEBDODIB: number;
+    NMGBGGFJBIA: number;
+    NGEMPNOFHLJ: string;
+    sideIconName: string;
+    CEABIFHHIKH: number;
+}
+
+export interface INamecardExcelData {
+    interactionTitleTextMapHash: number;
+    materialType: string;
+    stackLimit: number;
+    maxUseCount: number;
+    useOnGain: boolean;
+    noFirstGetHint: boolean;
+    itemUse: unknown[];
+    rankLevel: number;
+    effectDescTextMapHash: number;
+    specialDescTextMapHash: number;
+    typeDescTextMapHash: number;
+    effectIcon: string;
+    effectName: string;
+    picPath: string[];
+    satiationParams: any[];
+    destroyReturnMaterial: any[];
+    destroyReturnMaterialCount: any[];
+    isForceGetHint: boolean;
+    id: number;
+    nameTextMapHash: number;
+    descTextMapHash: number;
+    icon: string;
+    itemType: string;
+}
+
+export interface IReliquaryAffixExcelData {
+    affixId: number;
+    id: number;
+    level: number;
+    nameTextMapHash: number;
+    descTextMapHash: number;
+    openConfig: string;
+    addProps: unknown[];
+    paramList: number[];
+}
+
+export interface IReliquarySetExcelData {
+    setId: number;
+    setIcon: string;
+    setNeedNum: number[];
+    EquipAffixId: number;
+    containsList: number[];
+    PJBFOFFGFJD: number;
+    JNBEOBPDNND: any[];
+    textList: number[];
+}
+
+export interface INameTextMapHash {
+    nameTextMapHash: number;
+}
